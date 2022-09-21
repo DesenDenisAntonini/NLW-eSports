cad05f7e3569994378d2387db1c9c1dafae539a4
Denis Antonini <denis.oliveira@anyitsolutions.com.br>
2022-09-21 19:12:39 -0300

    feat: construction of the DUO with implementation of the back end MOBILE

--- mobile/src/components/DuoCard/index.tsx
+++ mobile/src/components/DuoCard/index.tsx
@@ -1,5 +1,9 @@
-import { View } from 'react-native';
+import { Text, TouchableOpacity, View } from 'react-native';
+import { GameController } from 'phosphor-react-native';
+
 import { DuoInfo } from '../DuoInfo';
+
+import { THEME } from '../../theme';
 
 import { styles } from './styles';
 
@@ -14,10 +18,11 @@ export interface DuoCardProps{
 }
 
 interface Props {
-  data: DuoCardProps
+  data: DuoCardProps;
+  onConnect: () => void;
 }
 
-export function DuoCard({ data }: Props) {
+export function DuoCard({ data, onConnect }: Props) {
   return (
     <View style={styles.container}>
       <DuoInfo
@@ -30,12 +35,28 @@ export function DuoCard({ data }: Props) {
       />
       <DuoInfo
         label="Disponibilidade"
-        value={`${data.weekDays.length}dias`}
+        value={`${data.weekDays.length}dias \u2022 ${data.hourStart} - ${data.hourEnd}`}
       />
       <DuoInfo
-        label="Nome"
-        value="Diego Fernandes"
+        label="Chamada de áudio"
+        value={data.useVoiceChannel ? "Sim" : "Não"}
+        colorValue={data.useVoiceChannel ? THEME.COLORS.SUCCESS : THEME.COLORS.ALERT}
       />
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={onConnect}
+      >
+        <GameController 
+          color={THEME.COLORS.TEXT}
+          size={20}
+        />
+
+        <Text style={styles.buttonTitle}>
+          Conectar
+        </Text>
+
+      </TouchableOpacity>
 
     </View>
   );
